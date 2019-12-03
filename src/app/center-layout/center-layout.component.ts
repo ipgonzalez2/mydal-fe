7dfad839d0ea535a6a7a6878e8b70b7bc23cb0e6
nghervella <nghervella@esei.uvigo.es>
2019-12-03 13:23:07 +0100

    Resuelto problema ficheros

--- src/app/center-layout/center-layout.component.ts
+++ src/app/center-layout/center-layout.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Folder } from '../shared/models/folder.model';
 import { FolderService } from '../shared/services/folder.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { File } from '../shared/models/file.model';
+import { FileService } from '../shared/services/file.service';
 
 @Component({
   selector: 'app-center-layout',
@@ -12,8 +14,9 @@ export class CenterLayoutComponent implements OnInit {
 
   foldersArray : Folder[];
   currentFolderId:number;
+  filesArray : File[];
   
-    constructor(private folderService : FolderService) { 
+    constructor(private folderService : FolderService, private fileService : FileService) { 
     }
     
     ngOnInit() {
@@ -21,12 +24,20 @@ export class CenterLayoutComponent implements OnInit {
       this.currentFolderId=null;
 
       this.refreshFolders();
+      this.refreshFiles();
     }
 
     refreshFolders(){
       this.folderService.getFolders(this.currentFolderId)
       .subscribe(
         data=> this.foldersArray = data["response"]
+      );
+    }
+
+    refreshFiles(){
+      this.fileService.getFiles(this.currentFolderId)
+      .subscribe(
+        data=> this.filesArray = data["response"]
       );
     }
 

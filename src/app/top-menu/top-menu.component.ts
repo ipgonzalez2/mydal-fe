c7223bd12fbebe1f54a049bf298651678415ca9c
nghervella <nghervella@esei.uvigo.es>
2019-12-03 10:43:43 +0100

    Actualización

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/models/user.model';
 
 @Component({
   selector: 'app-top-menu',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-menu.component.scss']
 })
 export class TopMenuComponent implements OnInit {
-
+public user:User;
   constructor() { }
 
   ngOnInit() {
+    this.user=JSON.parse(localStorage.getItem('currentUser'));
   }
 
 }

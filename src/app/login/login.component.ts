debbd9ecd59e009c5aa684ae161f8ff206aa3d67
nghervella <nghervella@esei.uvigo.es>
2020-01-13 13:12:28 +0100

    Toasts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,7 +3,7 @@ import { User } from '../shared/models/user.model';
 import { UsersService } from '../shared/services/users.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AuthenticationService } from '../shared/services/authentication.service';
-
+import { ToastrService, ToastrConfig } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -18,7 +18,12 @@ export class LoginComponent implements OnInit {
   constructor(private usersService:UsersService, 
     private router: Router, 
     private route: ActivatedRoute,
-    private auth:AuthenticationService) { 
+    private auth:AuthenticationService,
+    private toastr: ToastrService) { 
+    this.toastr.toastrConfig.disableTimeOut = false;
+    this.toastr.toastrConfig.extendedTimeOut;
+    this.toastr.toastrConfig.tapToDismiss = false;
+    this.toastr.toastrConfig.closeButton = true;
     
     this.route.queryParams.subscribe(
       data => {
@@ -42,34 +47,62 @@ export class LoginComponent implements OnInit {
   }
 
   public login(){
+    if(this.user.email=="" || this.user.email == null){
+      this.toastr.error("El email es obligatorio","Error de logueo")
+    }else{
+      if(this.user.password=="" || this.user.password == null){
+        this.toastr.error("La contraseña es obligatoria","Error de logueo")
+      }else{
+        this.auth.login(this.user.email,this.user.password).subscribe(
+          d=>{
+    
+              this.register = true;
+              this.user.username="";
+              this.user.email=""; 
+              this.user.password= "";
+    
+              this.router.navigate([this.url]);
+          },
+          
+          e=>this.toastr.error(e["error"]["error"], "Error de autenticación")
+          
+          
+        );
+      }
+    }
 
-    this.auth.login(this.user.email,this.user.password).subscribe(
-      d=>{
-
-          this.register = true;
-          this.user.username="";
-          this.user.email=""; 
-          this.user.password= "";
-
-          this.router.navigate([this.url]);
-      },
-      e=>console.error(e)
-    );
+    
 
   }
 
   public registerUser(){
+    
+    
+    if(this.user.username=="" || this.user.username == null){
+      this.toastr.error("El nombre es obligatorio","Error de registro")
+    }else{
+      if(this.user.email=="" || this.user.email == null){
+        this.toastr.error("El email es obligatorio","Error de registro")
+      }else{
+        if(this.user.password=="" || this.user.password == null){
+          this.toastr.error("La contraseña es obligatoria","Error de registro")
+        }else{
+          this.usersService.registerUser(this.user).subscribe(
+            d=>{
+                
+                this.register = false;
+                this.user.username="";
+                this.user.password= "";
+               
+            },
+            e=>this.toastr.error(e["error"]["response"], "Error de registro")
+          );
+        }
+      }
+      
+    }
 
-    this.usersService.registerUser(this.user).subscribe(
-      d=>{
-          
-          this.register = false;
-          this.user.username="";
-          this.user.password= "";
-         
-      },
-      e=>console.error(e)
-    );
+   
 
   }
 
